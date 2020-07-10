db1c8d61026201bce4a011262a6c31de5416764d
Jeremias Diaz <monitz87@gmail.com>
2020-07-10 09:32:19 -0400

    feat: initial sample code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,58 @@
-// Happy Coding!
-console.log('Polymath Rocks! (It would rock a whole lot less without Jere, though)');
+import { Polymesh } from '@polymathnetwork/polymesh-sdk';
+import { KnownTokenType } from '@polymathnetwork/polymesh-sdk/types';
+import BigNumber from 'bignumber.js';
+
+/**
+ * Polymesh SDK connection
+ */
+async function run(): Promise<void> {
+  console.log('Connecting to the node...\n\n');
+  const api = await Polymesh.connect({
+    nodeUrl: 'wss://pme.polymath.network',
+    accountUri: '//Alice',
+    middleware: {
+      /* eslint-disable @typescript-eslint/no-non-null-assertion */
+      link: process.env.MIDDLEWARE_URL!,
+      key: process.env.MIDDLEWARE_KEY!,
+      /* eslint-enable @typescript-eslint/no-non-null-assertion */
+    },
+  });
+
+  const ticker = 'DEMO_TOKEN';
+  const reservationQueue = await api.reserveTicker({ ticker }); // transaction queue, contains a list of transactions to be run
+  const tickerReservation = await reservationQueue.run(); // will ask for signature on each transaction in the queue
+
+  console.log('Ticker reserved!\n\n');
+
+  const reservationDetails = await tickerReservation.details();
+
+  console.log('RESERVATION INFO:\n');
+  console.log('Expiry:', reservationDetails.expiryDate);
+  console.log('Owner DID:', reservationDetails.owner?.did);
+  console.log('Status:', reservationDetails.status);
+
+  const creationQueue = await tickerReservation.createToken({
+    name: 'My Demo Token',
+    isDivisible: true,
+    totalSupply: new BigNumber(3000000),
+    tokenType: KnownTokenType.EquityCommon,
+  });
+  const securityToken = await creationQueue.run();
+
+  console.log('Token created!');
+
+  /*
+    Other areas to explore:
+
+    - securityToken.documents
+    - securityToken.transfers
+    - securityToken.tokenHolders
+    - securityToken.issuance
+    - securityToken.compliance
+    - securityToken.compliance.rules
+
+    Each of these is a namespace containing subsets of the token's functionality. For example, 
+    to issue tokens to your holders, you would call securityToken.issuance.issue
+  */
+}
+run().catch(err => console.log(err));
